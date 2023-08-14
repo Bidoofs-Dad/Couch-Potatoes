e3f29ea5e678c6aec95924899e7527cdc14e4050
Zane Werle <zanewerle@gmail.com>
2023-08-13 21:21:36 -0500

    found the issue! I had a couple spots where 'user' was wrongly capitalized one one where it should have been capitalized. Will add css in the future!

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
@@ -7,7 +7,7 @@ import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error, data }] = useMutation(LOGIN_USER);
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -45,12 +45,6 @@ const Login = (props) => {
         <div className="card">
           <h4 className="card-header bg-dark text-light p-2">Login</h4>
           <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
               <form onSubmit={handleFormSubmit}>
                 <input
                   className="form-input"
@@ -76,7 +70,6 @@ const Login = (props) => {
                   Submit
                 </button>
               </form>
-            )}
 
             {error && (
               <div className="my-3 p-3 bg-danger text-white">
