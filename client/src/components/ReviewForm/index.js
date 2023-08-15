d1cbe1018d579f8ba9bb0feac7a3f94d0997c90f
Zane Werle <zanewerle@gmail.com>
2023-08-14 23:21:17 -0500

    cut out bootstrap css, since we are not using it (just out of game.js and reviewform though)

--- client/src/components/ReviewForm/index.js
+++ client/src/components/ReviewForm/index.js
@@ -67,29 +67,27 @@ const ReviewForm = () => {
       {Auth.loggedIn() ? (
         <>
           <p
-            className={`m-0 ${
+            className={`${
               characterCount === 280 || error ? 'text-danger' : ''
             }`}
           >
             Character Count: {characterCount}/280
           </p>
           <form
-            className="flex-row justify-center justify-space-between-md align-center"
             onSubmit={handleFormSubmit}
           >
-            <div className="col-12 col-lg-9">
+            <div>
               <textarea
                 name="reviewText"
                 placeholder="Whatcha think about this game??"
                 value={reviewText}
-                className="form-input w-100"
                 style={{ lineHeight: '1.5', resize: 'vertical' }}
                 onChange={handleChange}
               ></textarea>
             </div>
 
-            <div className="col-12 col-lg-3">
-              <button className="btn btn-primary btn-block py-3" type="submit">
+            <div>
+              <button type="submit">
                 Add Review
               </button>
             </div>
