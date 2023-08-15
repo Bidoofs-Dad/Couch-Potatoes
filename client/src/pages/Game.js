25a882a27b499521c39fd90e8739bd4ef29bd061
Zane Werle <zanewerle@gmail.com>
2023-08-14 19:56:51 -0500

    added in the fetch request that will look for a specific game, based off of it's ID!

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Game = () => {
-return (
-    <main>
-        <div className="game-con">
-            <div className="game-pic">
-                <div className="game-name"></div>
+    const { gameId } = useParams();
+    const [gameDetails, setGameDetails] = useState(null);
+
+    return (
+        <main>
+            <div className="game-con">
+                <div className="game-pic">
+                    <div className="game-name"></div>
+                </div>
+
+                <div className='game-rating1'></div>
+                <div className='game-rating2'></div>
             </div>
 
-            <div className='game-rating1'></div>
-            <div className='game-rating2'></div>
-        </div>
+            <div className='review-con'>
+                <form>
+                    <input
+                        className="review-input"
+                        placeholder="Your Review"
+                        name="review"
+                        type="review"
+                    />
+                    <button
+                        className="btn btn-block btn-primary"
+                        style={{ cursor: 'pointer' }}
+                        type="submit"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
 
-        <div className='review-con'>
-        <form>
-                <input
-                  className="review-input"
-                  placeholder="Your Review"
-                  name="review"
-                  type="review"
-                />
-                <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-        </div>
-
-    </main>
+        </main>
     );
 };
 
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,7 @@
 export const searchGames = (query) => {
   return fetch(`https://api.rawg.io/api/games?search=${query}&key=ac7de14847e84d37be3b60940720db8c`);
 };
+
+export const individualGame = (query) => {
+  return fetch(`https://api.rawg.io/api/games/${query}?key=ac7de14847e84d37be3b60940720db8c`);
+};
