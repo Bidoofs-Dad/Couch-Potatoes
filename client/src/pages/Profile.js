ebd5a52cd580c3aa9476fa1a9e760dc544a6150c
Zane Werle <zanewerle@gmail.com>
2023-08-15 01:38:13 -0500

    temp signup link from profile, to properlly test heroku

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import { QUERY_USERS, QUERY_ME } from '../utils/queries';
@@ -27,7 +28,7 @@ const Profile = () => {
     return (
       <h4>
         You need to be logged in to see this. Use the navigation links above to
-        sign up or log in!
+        <Link to="/signup">sign</Link> up or log in!
       </h4>
     );
   }
