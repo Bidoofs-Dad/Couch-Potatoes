67210dbc97954e611cd9c9a0b3710741ec92e202
Jayden Schuffenhauer <jdrich135@gmail.com>
2023-08-17 16:58:45 -0600

    added api key env

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,13 @@
+const apiKey = process.env.REACT_APP_API_KEY;
+
 export const searchGames = (query) => {
-  return fetch(`https://api.rawg.io/api/games?search=${query}&key=ac7de14847e84d37be3b60940720db8c`);
+  return fetch(`https://api.rawg.io/api/games?search=${query}&key=${apiKey}`);
 };
 
 export const individualGame = (query) => {
   return fetch(`https://api.rawg.io/api/games/${query}?key=ac7de14847e84d37be3b60940720db8c`);
 };
+
+
+
+
