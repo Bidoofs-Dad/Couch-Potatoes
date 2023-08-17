2a57e0abc371ff164aa6c210d84c7bce61c8dbf8
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-08-17 16:33:16 -0500

    Added .env

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,4 +1,7 @@
-export const searchGames = (query) => {
+require('dotenv').config();
+
+export const 
+searchGames = (query) => {
   return fetch(`https://api.rawg.io/api/games?search=${query}&key=ac7de14847e84d37be3b60940720db8c`);
 };
 
