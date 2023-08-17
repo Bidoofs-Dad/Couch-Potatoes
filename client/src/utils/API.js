9ee4ab29fec3da3151caa52e17b34e4a865685ed
Zane Werle <zanewerle@gmail.com>
2023-08-17 16:13:36 -0500

    added in the 4 different api calls for the homepage into the API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,3 +5,19 @@ export const searchGames = (query) => {
 export const individualGame = (query) => {
   return fetch(`https://api.rawg.io/api/games/${query}?key=ac7de14847e84d37be3b60940720db8c`);
 };
+
+export const fetchHorror = () => {
+  return fetch(`https://api.rawg.io/api/games?page_size=40&tags=16&key=ac7de14847e84d37be3b60940720db8c`);
+};
+
+export const fetchIndie = () => {
+  return fetch(`https://api.rawg.io/api/games?page_size=40&genres=51&key=ac7de14847e84d37be3b60940720db8c`);
+};
+
+export const fetchBethesda = () => {
+  return fetch(`https://api.rawg.io/api/games?page_size=40&developers=4207&key=ac7de14847e84d37be3b60940720db8c`);
+};
+
+export const fetchMultiplayer = () => {
+  return fetch(`https://api.rawg.io/api/games?page_size=40&tags=7&key=ac7de14847e84d37be3b60940720db8c`);
+};
