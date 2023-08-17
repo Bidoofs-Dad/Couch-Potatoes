8adcf9f402e62c79af252461c7126b353e9deaa7
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-08-16 20:25:25 -0500

    Updated Review List

--- client/src/components/ReviewList/index.js
+++ client/src/components/ReviewList/index.js
@@ -1,36 +1,51 @@
 import React from 'react';
-
-const reviewList = ({ review = [] }) => {
-    if (!reviews.length) {
-        return <h3>No Reviews Yet!</h3>;
-    }
-
-    return (
-        <>
-            <h3
-                className="p-5 display-inline-block"
-                style={{ borderBottom: '1px dotted #1a1a1a' }}
-            >
-                Reviews
-            </h3>
-            <div className="flex-row my-4">
-                {reviews &&
-                comments.map((comment) =>(
-                    <div key={review.id} className="col-12 mb-3 pb-3">
-                        <div className="p-3 bg-dark text-light">
-                            <h5 className="card-header">
-                                {review.reviewAuthor} reviewed{' '}
-                                <span style={{ fontSize: '1.0rem' }}>
-                                on {review.createdAt}
-                                </span>
-                            </h5>
-                            <p className="card-body">{review.reviewText}</p>
-                        </div>
-                    </div>
-                ))}                
+import { Link } from 'react-router-dom';
+const ReviewList = ({
+  reviews,
+  title,
+  showTitle = true,
+  showUsername = true,
+}) => {
+  if (!reviews.length) {
+    return <h3>No Reviews Yet</h3>;
+  }
+  return (
+    <div>
+      {showTitle && <h3>{title}</h3>}
+      {reviews &&
+        reviews.map((review) => (
+          <div key={review._id} className="card mb-3">
+            <h4 className="card-header bg-primary text-light p-2 m-0">
+              {showUsername ? (
+                <Link
+                  className="text-light"
+                  to={`/profiles/${review.reviewUser}`}
+                >
+                  {review.reviewUser} <br />
+                  <span style={{ fontSize: '1rem' }}>
+                    left this review on {review.dateCreated}
+                  </span>
+                </Link>
+              ) : (
+                <>
+                  <span style={{ fontSize: '1rem' }}>
+                    You left this review on {review.dateCreated}
+                  </span>
+                </>
+              )}
+            </h4>
+            <div className="card-body bg-light p-2">
+              <p>{review.reviewText}</p>
             </div>
-        </>
-    );
+            <Link
+              className="btn btn-primary btn-block btn-squared"
+              to={`/reviews/${review._id}`}
+            >
+              Whatcha gotta say about that??
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
 };
-
 export default ReviewList;
